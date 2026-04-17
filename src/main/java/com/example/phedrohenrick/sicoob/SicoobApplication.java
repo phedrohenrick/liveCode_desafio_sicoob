86cc5b6d423c6ba3850925c05a3c5a7fdeee2cb7
phedrohenrick <phedrohenrickdasilva@gmail.com>
2026-04-17 11:05:38 -0300

    first commit

---
+++ src/main/java/com/example/phedrohenrick/sicoob/SicoobApplication.java
@@ -0,0 +1,13 @@
+package com.example.phedrohenrick.sicoob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicoobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicoobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phedrohenrick/sicoob/SicoobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phedrohenrick.sicoob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicoobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
